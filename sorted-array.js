cfc72c664c33143d72e89576e47bf75c703b5906
Nirmal Kumar <84436580+Nirmal-Kumar97@users.noreply.github.com>
2022-07-08 10:50:26 +0530

    add

--- sorted-array.js
+++ sorted-array.js
@@ -1,7 +1,15 @@
 const arr1 = [1, 2, 3, 5, 6, 7];
 const arr2 = [6, 3];
 const commonValue = [];
-arr1.forEach(num1 => {
-    arr2.forEach(num2 => num1 === num2 && commonValue.push(num1))
+arr1.map(num1 => {
+    arr2.map(num2 => num1 === num2 && commonValue.push(num1))
 })
 console.log(commonValue);
+
+const arr3 = [1, 2, 3, 5, 6, 7];
+const arr4 = [6, 3];
+const commonValues = [];
+arr3.forEach(num3 => {
+    arr4.forEach(num4 => num3 === num4 && commonValues.push(num3))
+})
+console.log(commonValues);
